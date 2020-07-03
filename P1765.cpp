70985e1cf75e00112f3bd5b90c5d3b6757e91a58
icantenter <2775734866@qq.com>
2020-07-03 10:23:40 +0800

    p1765

---
+++ P1765.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+#include <map>
+int main()
+{
+    std::string msg;
+    int counter = 0;
+    int temp;
+    const std::string keyboard[12] = {
+        "",     "abc",  "def", 
+        "ghi",  "jkl",  "mno", 
+        "pqrs", "tuv",  "wxyz", 
+        "*",    " ",    "#"};
+    getline(std::cin , msg);
+
+    for (std::string::iterator i = msg.begin(); i < msg.end(); i++)
+    {
+        for (int j = 0; j < 12; j++)
+        {
+            if ((temp = keyboard[j].find(*i)) != std::string::npos)
+            {
+                counter += temp + 1;
+            }
+        }
+    }
+    std::cout << counter;
+
+    system("pause");
+    return 0;
+}
